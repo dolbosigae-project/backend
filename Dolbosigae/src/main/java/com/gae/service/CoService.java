6a96033d63264ef4af379032ac3ebe690a800d99
chlwogur8520 <chlwogur8520@naver.com>
2024-07-11 16:47:56 +0900

    `12

--- Dolbosigae/src/main/java/com/gae/service/CoService.java
+++ Dolbosigae/src/main/java/com/gae/service/CoService.java
@@ -2,7 +2,15 @@ package com.gae.service;
 
 import org.springframework.stereotype.Service;
 
+import com.gae.mapper.CoMapper;
+
 @Service
 public class CoService {
+	private CoMapper mapper;
 
+	public CoService(CoMapper mapper) {
+		this.mapper = mapper;
+	}
+	
+	
 }
