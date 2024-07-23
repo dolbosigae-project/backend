87c81b5b8f711980e6bcc0526c33cf4357b3f3ee
Yu yeong Park <162700461+YuKorea@users.noreply.github.com>
2024-07-23 18:20:21 +0900

    Add files via upload
    0723yu

---
+++ Dolbosigae/src/main/java/com/gae/mapper/PHMapper.java
@@ -0,0 +1,16 @@
+package com.gae.mapper;
+
+import com.gae.dto.PHDTO;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface PHMapper {
+    List<PHDTO> getPhList(@Param("startRow") int startRow, @Param("endRow") int endRow);
+    int getTotalCount();
+    PHDTO selectPhInfo(@Param("phId") int phId);
+    List<PHDTO> searchPh(@Param("phText") String phText, @Param("startRow") int startRow, @Param("endRow") int endRow);
+    int getTotalCountBySearch(@Param("phText") String phText);
+    int deletePh(@Param("phId") int phId);
+}
