78ff137aa6f37c2816216d8d502933dff2b25c5f
Yu yeong Park <162700461+YuKorea@users.noreply.github.com>
2024-07-23 16:11:21 +0900

    Update HOController.java
    0723

--- Dolbosigae/src/main/java/com/gae/controller/HOController.java
+++ Dolbosigae/src/main/java/com/gae/controller/HOController.java
@@ -28,27 +28,34 @@ public class HOController {
     public Map<String, Object> selectHoList(
         @RequestParam(defaultValue = "1") int page,
         @RequestParam(defaultValue = "5") int limit,
-        @RequestParam(required = false) String plText) {
-        
+        @RequestParam(required = false) String hoText) {
+
         Map<String, Object> map = new HashMap<>();
         try {
-            System.out.println("Fetching data with params - page: " + page + ", limit: " + limit + ", plText: " + plText);
             HOResponseVo response;
-            if (plText == null || plText.isEmpty()) { // hoText를 plText로 변경
+            if (hoText == null || hoText.isEmpty()) { 
                 response = hoService.getHoList(page, limit);
             } else {
-                response = hoService.searchHo(plText, page, limit); // hoText를 plText로 변경
+                response = hoService.searchHo(hoText, page, limit); 
             }
+
             map.put("contents", response.getContents());
             map.put("pagination", response.getPagination());
-            System.out.println("Pagination info: " + response.getPagination()); // 로그 추가
         } catch (Exception e) {
             e.printStackTrace();
             map.put("error", "Internal Server Error");
         }
         return map;
     }
-
+    
+    @GetMapping("/hoinfo")
+    public HODTO selectHoInfo(@RequestParam int hoId) {
+        System.out.println(hoId);
+        HODTO result = hoService.selectHoInfo(hoId);
+        System.out.println(result);
+        return result;
+    }
+    
     @DeleteMapping("/hospitals/delete/{hoId}")
     public Map<String, String> deleteHospitals(@PathVariable int hoId) {
         Map<String, String> response = new HashMap<>();
