e3e44cfbebc6202d9451d8674e00ad91674fd89e
sumin <ksm7058@naver.com>
2024-07-28 16:24:31 +0900

    0728 취합 (김우진)

--- Dolbosigae/src/main/java/com/gae/mapper/ShelterMapper.java
+++ Dolbosigae/src/main/java/com/gae/mapper/ShelterMapper.java
@@ -4,14 +4,16 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.gae.dto.ShelterDTO;
 
 @Mapper
 public interface ShelterMapper {
-
-	List<ShelterDTO> selectShelterList(Map<String, Object> map);
-
-	int selectShelterTotalCount();
-
+    List<ShelterDTO> selectShelterList(Map<String, Object> map);
+    void insertShelter(ShelterDTO shelterDTO);
+    void deleteShelter(@Param("id") String id);
+    ShelterDTO selectShelterById(@Param("id") String id);
+    int selectShelterTotalCount(Map<String, Object> filterParams);
+    List<ShelterDTO> selectAllShelters();
 }
