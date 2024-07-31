9b62f288787a4bd4db902c25fb1934eaed146955
suminkim0 <ksm7058@naver.com>
2024-07-31 18:18:15 +0900

    shelter2

--- Dolbosigae/src/main/java/com/gae/service/ShelterService.java
+++ Dolbosigae/src/main/java/com/gae/service/ShelterService.java
@@ -19,6 +19,7 @@ public class ShelterService {
 		this.shelterMapper = shelterMapper;
 	}
 
+	//전체 리스트 조회	
 	public ShelterResonseVo getShelterList(int page) {
 		int pageOfContentCount = 10; //페이지당 센터 수
 		int totalCount = shelterMapper.getTotalCount(); //전체 센터 수 가져오기
@@ -31,7 +32,23 @@ public class ShelterService {
 		return new ShelterResonseVo(shelters, paggingVo);
 	}
 
-	
-	
-	
+	//검색 결과 조회
+	public List<ShelterDTO> searchShelters(String category, String term) {
+		if(category.equals("지역선택")) {
+			return shelterMapper.searchByAllRegion(term);
+		} else {
+			return shelterMapper.searchByRegionName(category, term);
+		}
+    }
+
+	//특정 센터 상세
+	public ShelterDTO getShelterDetail(String shelterId) {
+		ShelterDTO shelterDetail = shelterMapper.getShelterDetail(shelterId);
+		return shelterDetail;
+	}
 }
+
+	
+	
+	
+
