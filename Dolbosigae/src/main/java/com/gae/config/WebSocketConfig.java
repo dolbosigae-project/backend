b3c8d9cf2d40438848aec35069785abdf7a0fb95
suminkim0 <ksm7058@naver.com>
2024-08-05 11:58:47 +0900

    aws 배포

--- Dolbosigae/src/main/java/com/gae/config/WebSocketConfig.java
+++ Dolbosigae/src/main/java/com/gae/config/WebSocketConfig.java
@@ -22,7 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("https://dolbosigae.vercel.app/").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("https://dolbosigae.site").withSockJS();
     }
     
     
